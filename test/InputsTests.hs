eece1e85f111c517ec4aca791dd945e411d8fea9
Joseph Canero <jmc41493@gmail.com>
2017-07-23 16:57:56 -0400

    added metadata test for inputs.

--- test/InputsTests.hs
+++ test/InputsTests.hs
@@ -5,7 +5,7 @@ module InputsTests
   inputsTests
 ) where
 
-import           Control.Lens
+import           Control.Lens                     hiding ((.=))
 import           Data.Aeson
 import           Data.HashMap.Lazy                as M
 import           Data.Text                        (Text)
@@ -17,6 +17,7 @@ inputsTests :: [Test]
 inputsTests = [
     TestCase basicInput
   , TestCase inputWithConcepts
+  , TestCase inputWithMetadata
   ]
 
 basicInput :: Assertion
@@ -55,5 +56,32 @@ inputWithConcepts = expected @=? toJSON (input (Url "test.png") & concepts .~ te
                               Object $ M.fromList [("id", "c1"), ("value", Bool True)]
                             , Object $ M.fromList [("id", "c2"), ("value", Bool False)]
                             ])
+            ])
+        ]
+
+inputWithMetadata :: Assertion
+inputWithMetadata = expected @=? toJSON (input (Url "test.png") & metadata .~ testMeta)
+  where
+    testMeta = ["extra" .= Object extraMeta, "name" .= String "My Test Image"]
+    extraMeta = M.fromList [
+          ("size", Number 1024)
+        , ("type", String "png")
+        ]
+    expected =
+      Object $
+        M.fromList [
+          ("data", Object $
+            M.fromList [
+              ("image", Object $
+                M.fromList [
+                  ("allow_duplicate_url", Bool False)
+                , ("url", String "test.png")
+                ])
+            , ("concepts", Array V.empty)
+            , ("metadata", Object $
+                           M.fromList [
+                              ("extra", Object extraMeta)
+                            , ("name", String "My Test Image")
+                           ])
             ])
         ]
